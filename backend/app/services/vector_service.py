e53c4bfbe41467516edb9a394fe60ad97a114b44
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-07-29 11:46:42 +0530

    application running

--- backend/app/services/vector_service.py
+++ backend/app/services/vector_service.py
@@ -1,50 +1,36 @@
 from sqlalchemy.orm import Session
+
 from app.models.document_chunk import DocumentChunk
 
 
-
 def save_chunks(
-        db:Session,
-        document_id:int,
-        chunks:list[str],
-        embeddings:list[list[float]]
-        ):
-
+    db: Session,
+    document_id: int,
+    chunks: list[str],
+    embeddings: list[list[float]]
+):
     """
-    Save text chunks and their embeddings into the database.
-
-    Args:
-        db (Session): Database session.
-        document_id (int): Uploaded document ID.
-        chunks (list[str]): List of text chunks.
-        embeddings (list[list[float]]): List of embedding vectors.
+    Save all document chunks and embeddings into PostgreSQL.
     """
 
+    if len(chunks) != len(embeddings):
+        raise ValueError(
+            "Number of chunks and embeddings must be equal."
+        )
 
-    if len(chunks) != len(embedding):
-         raise ValueError(
-          "Number of chunks and embeddings must be equal."
-     )
+    document_chunks = []
 
+    for index, (chunk, embedding) in enumerate(zip(chunks, embeddings)):
+        document_chunk = DocumentChunk(
+            document_id=document_id,
+            chunk_index=index,
+            chunk_text=chunk,
+            embedding=embedding
+        )
 
+        document_chunks.append(document_chunk)
 
-    document_chunk=[]
+    db.add_all(document_chunks)
+    db.commit()
 
-    for index,(chunk,embedding) in enumerate(zip(chunks,embeddings)):
-
-         document_chunk=DocumentChunk(
-              document_id=document_id,
-              chunk_index=index,
-              chunk_text=chunk,
-              embedding=embedding
-         )
-
-         document_chunk.append(document_chunk)
-
-
-         db.add_all(document_chunk)
-         db.commit()
-
-         return document_chunk
-
-
+    return document_chunks
