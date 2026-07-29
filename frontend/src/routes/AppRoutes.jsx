97982939b66e98d4517e8af41d1d5bef467c40b4
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-07-29 16:34:52 +0530

    app routes file done

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,33 @@
+import {BroweserRouter as Routes, Route, BrowserRouter } from  "react-router-dom";
+
+import  Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
+
+import Upload from "../pages/Upload";
+
+import Documents from "../pages/Documents";
+
+import Chat from "../pages/Chat";
+import Profile from "../pages/Profile";
+import NotFound from "../pages/NotFound";
+
+
+
+const AppRoutes = () => {
+    return(
+        <BrowserRouter>
+        <Routes>
+            <Route path="/"element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/upload" element={<Upload />} />
+            <Route path="/documents" element={<Documents />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+        
+        </BrowserRouter>
+    )
+}
+             
+export default AppRoutes;
